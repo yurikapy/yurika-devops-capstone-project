9ab57502741c37c687cc0eb4485004695f27f25d
yurikapy <76867390+yurikapy@users.noreply.github.com>
2024-12-22 01:27:06 +0700

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -12,6 +12,7 @@ from tests.factories import AccountFactory
 from service.common import status  # HTTP Status Codes
 from service.models import db, Account, init_db
 from service.routes import app
+from service import talisman
 
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
