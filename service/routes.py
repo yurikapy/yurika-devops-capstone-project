112898fc07dff12c65c2e6b6ccf3684664193cac
yurikapy <76867390+yurikapy@users.noreply.github.com>
2024-12-21 20:13:19 +0700

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -104,7 +104,7 @@ def check_content_type(media_type):
     # READ AN ACCOUNT
     ######################################################################
     @app.route("/accounts/<int:account_id>", methods=["GET"])
-    def get_accounts(account_id):
+def get_accounts(account_id):
         """
         Reads an Account
         This endpoint will read an Account based the account_id that is requested
@@ -116,3 +116,8 @@ def check_content_type(media_type):
             abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
         return account.serialize(), status.HTTP_200_OK
+    )
+    def test_get_account_not_found(self):
+        """It should not Read an Account that is not found"""
+        resp = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
