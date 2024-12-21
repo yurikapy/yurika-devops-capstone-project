1f1ca72011199fb60187567ecf274857edb194bc
yurikapy <76867390+yurikapy@users.noreply.github.com>
2024-12-22 03:56:14 +0700

    Update config.py

--- service/config.py
+++ service/config.py
@@ -1,8 +1,4 @@
-"""
-Global Configuration for Application
-"""
 import os
-
 
 # Get configuration from environment
 DATABASE_URI = os.getenv("DATABASE_URI")
@@ -12,13 +8,13 @@ if not DATABASE_URI:
     DATABASE_USER = os.getenv("DATABASE_USER", "postgres")
     DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "postgres")
     DATABASE_NAME = os.getenv("DATABASE_NAME", "postgres")
-    DATABASE_HOST = os.getenv("DATABASE_HOST", "localhost")
-    DATABASE_URI = "postgresql://postgres:postgres@localhost:5432/postgres"
-    
+    DATABASE_HOST = os.getenv("DATABASE_HOST", "some-postgres")  # Ganti localhost menjadi some-postgres
+    DATABASE_URI=postgresql://postgres:postgres@some-postgres:5432/postgres
 
 # Configure SQLAlchemy
 SQLALCHEMY_DATABASE_URI = DATABASE_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Secret for session management
-SECRET_KEY = os.getenv("SECRET_KEY", "s3cr3t-key-shhhh")
+SECRET_KEY = os.getenv("SECRET_KEY", "s3cure_key_for_sessions")  # Jangan lupa untuk set SECRET_KEY
+r3t-key-shhhh")
