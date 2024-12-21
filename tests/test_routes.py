6445fb68548970bc80e56fef92166944118e58e4
yurikapy <76867390+yurikapy@users.noreply.github.com>
2024-12-21 20:51:34 +0700

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -123,7 +123,7 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-  def test_read_an_account(self):
+    def test_read_an_account(self):
         account_data = {
             "name": "John Doe",
             "email": "john.doe@example.com",
@@ -146,6 +146,35 @@ class TestAccountService(TestCase):
         self.assertEqual(data["name"], account_data["name"])
         self.assertEqual(data["email"], account_data["email"])
         self.assertEqual(data["balance"], account_data["balance"])
+
+    def test_get_account_list(self):
+        """It should Get a list of Accounts"""
+        self._create_accounts(5)  # Create 5 accounts for testing
+        resp = self.client.get(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 5)  # Verify that there are 5 accounts
+
+    def test_update_account(self):
+        """It should Update an existing Account"""
+        # create an Account to update
+        test_account = AccountFactory()
+        resp = self.client.post(BASE_URL, json=test_account.serialize())
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        # update the account
+        new_account = resp.get_json()
+        new_account["name"] = "Something Known"
+        resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        updated_account = resp.get_json()
+        self.assertEqual(updated_account["name"], "Something Known")
+
+    def test_delete_account(self):
+        """It should Delete an Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.delete(f"{BASE_URL}/{account.id}")
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
 
 if __name__ == '__main__':
     unittest.main()
