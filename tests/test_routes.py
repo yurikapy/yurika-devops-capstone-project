ed06c067bd52492a9619bd9837edf9ccffc97d57
yurikapy <76867390+yurikapy@users.noreply.github.com>
2024-12-21 21:02:49 +0700

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -175,6 +175,12 @@ class TestAccountService(TestCase):
         account = self._create_accounts(1)[0]
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+      
+    # Add the new test for method not allowed
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
 if __name__ == '__main__':
     unittest.main()
