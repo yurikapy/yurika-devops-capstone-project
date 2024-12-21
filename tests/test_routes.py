9d35baa4c4b23232ce544edd83471bf529ef16e1
yurikapy <76867390+yurikapy@users.noreply.github.com>
2024-12-22 01:12:05 +0700

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -182,5 +182,18 @@ class TestAccountService(TestCase):
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
+    def test_security_headers(self):
+        """It should return security headers"""
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        headers = {
+            'X-Frame-Options': 'SAMEORIGIN',
+            'X-Content-Type-Options': 'nosniff',
+            'Content-Security-Policy': 'default-src \'self\'; object-src \'none\'',
+            'Referrer-Policy': 'strict-origin-when-cross-origin'
+        }
+        for key, value in headers.items():
+            self.assertEqual(response.headers.get(key), value)
+
 if __name__ == '__main__':
     unittest.main()
